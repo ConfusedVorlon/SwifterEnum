239a6fe7c71e499730e2fb7ba55b290490993a53
Rob Jonson <rob@hobbyistsoftware.com>
2024-01-24 13:12:01 +0000

    define module immediately for base.rb

--- lib/swifter_enum/base.rb
+++ lib/swifter_enum/base.rb
@@ -1,29 +1,31 @@
-class SwifterEnum::Base
+module SwifterEnum
+  class Base
 
-  attr_reader :value
+    attr_reader :value
 
-  def self.values
-    {}
+    def self.values
+      {}
+    end
+
+    def initialize(value)
+      @value = value.to_sym
+    end
+
+    def ==(other)
+      other.instance_of?(self.class) && self.value == other.value
+    end
+
+    def t
+      I18n.t("swifter_enum.#{self.class.name.demodulize.underscore}.#{@value}")
+    end
+
+    def to_s
+      @value
+    end
+
+    def self.all
+      self.values.keys.map{|value| new(value)}
+    end
+
   end
-
-  def initialize(value)
-    @value = value.to_sym
-  end
-
-  def ==(other)
-    other.instance_of?(self.class) && self.value == other.value
-  end
-
-  def t
-    I18n.t("swifter_enum.#{self.class.name.demodulize.underscore}.#{@value}")
-  end
-
-  def to_s
-    @value
-  end
-
-  def self.all
-    self.values.keys.map{|value| new(value)}
-  end
-
 end
