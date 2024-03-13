f7424f9c751c86d8b42d69c5c898f152076bb26b
Rob Jonson <rob@hobbyistsoftware.com>
2024-03-13 10:15:54 +0000

    test extensions

--- test/swifter_enum_extensions_test.rb
+++ test/swifter_enum_extensions_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class SwifterEnumBasicsTest < Minitest::Test
+class SwifterEnumExtensionsTest < Minitest::Test
   # Getters
 
   # Emotion is an instance of EmotionEnum
@@ -8,6 +8,10 @@ class SwifterEnumBasicsTest < Minitest::Test
 
   def test_enum_read_value
     assert_equal SizeEnum.all_cases, [SizeEnum.new(:big),SizeEnum.new(:small)]
+  end
+
+  def test_to_s
+    assert_equal SizeEnum.new(:big).to_s, "big"
   end
 
 end
